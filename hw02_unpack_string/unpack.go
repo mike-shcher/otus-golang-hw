e38e9d548a25e0dc68aaf7381a6856798a0621f1
Mikhail Shcherbakov <MSHCHerbakov@artenecy.com>
2023-12-18 05:40:27 +0300

    HW2 is completed

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,57 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"regexp"
+	"strconv"
+	"strings"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(testString string) (string, error) {
+	re := regexp.MustCompile(`[^a-z0-9]+`)
+	if len(re.FindStringIndex(testString)) > 0 {
+		return "", ErrInvalidString
+	}
+	var resultString strings.Builder
+	bufferedChar := "initial value"
+
+	for i, char := range testString {
+		isDigit := false
+
+		numberOfRepetitions, err := strconv.Atoi(string(char))
+		if err != nil {
+			if bufferedChar == "initial value" ||
+				bufferedChar == "" &&
+					i != len(testString)-1 {
+				bufferedChar = string(char)
+
+				continue
+			}
+		} else {
+			isDigit = true
+		}
+
+		if i == 0 && isDigit {
+			return "", ErrInvalidString
+		}
+
+		if bufferedChar != "" && isDigit {
+			resultString.WriteString(strings.Repeat(bufferedChar, numberOfRepetitions))
+			bufferedChar = ""
+		} else if bufferedChar == "" && isDigit {
+			return "", ErrInvalidString
+		}
+
+		if bufferedChar != "" && !isDigit {
+			resultString.WriteString(bufferedChar)
+			bufferedChar = string(char)
+		}
+
+		if i == len(testString)-1 && !isDigit {
+			resultString.WriteString(string(char))
+		}
+	}
+
+	return resultString.String(), nil
 }
